e958979ebcd9960468e673e49ccc755ab0b117fc
kubkowski <jakub.opara@gmail.com>
2014-10-19 13:21:20 +0200

    Resolved some dependencies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  include ActionController::ImplicitRender
+  include ActionController::MimeResponds
 
   def cors_preflight_check
     headers['Access-Control-Max-Age'] = '1728000'
